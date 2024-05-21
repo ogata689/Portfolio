23eff39e5596c1d23c15f912aba0de759a9d78e6
ogata689 <83046490+ogata689@users.noreply.github.com>
2024-05-21 16:40:02 +0900

    Add files via upload
    テキスト内容の修正

--- conpoments/About.js
+++ conpoments/About.js
@@ -6,7 +6,7 @@ const AboutContainer = style.section`
   justify-content: center;
   align-items: center;
   margin: 0 auto;
-  padding: 200px 50px 200px 50px;
+  padding: 200px 150px 200px 50px;
   background: url(/images/about_bg.png);
   background-repeat: no-repeat;
   background-position: center;
@@ -30,6 +30,7 @@ const AboutTitle = style.h2`
   font-style: italic;
 `;
 const AboutText = style.p`
+padding-top: 10px;
   width: 400px;
   font-size: 16px;
   font-family: "Noto Sans JP", sans-serif;
@@ -39,11 +40,19 @@ export default function About(props) {
   return (
     <AboutContainer id="about">
       <div>
-        <AboutTitle>ABOUT</AboutTitle>
-        <AboutText>
-          こちらはAboutです
+        <AboutTitle>
+          Welcome
           <br />
-          Aboutが入る予定です
+          To My
+          <br />
+          Portfolio
+        </AboutTitle>
+        <AboutText>
+          こちらのWebサイトには私が今まで制作した様々な
+          <br />
+          Webサイトのデザインやバナー、 ポスター等の
+          <br />
+          広告デザインを掲載しております。
         </AboutText>
       </div>
       <figure>
